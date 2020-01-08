7657bfa2001883a521b7a5ab909cbe398baccb9e
Karthik Devaraja <kDevaraja@revspringinc.com>
2020-01-07 21:15:25 -0500

    created post and retrived post from mongoDB

--- backend/app.js
+++ backend/app.js
@@ -43,22 +43,14 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "1",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "2",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find()
+  .then(posts=>{
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: posts
+    });
+  })
+  
 });
 
 module.exports = app;
