8c5a6367bb514545624d24ed614e373fe4eefe41
Karthik Devaraja <kDevaraja@revspringinc.com>
2020-01-09 20:09:07 -0500

    Added pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -75,11 +75,25 @@ router.put(
 );
 
 router.get("", (req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = +req.query.pageSize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  let fetchedPosts;
+  if(pageSize && currentPage) {
+    postQuery
+    .skip(pageSize * (currentPage - 1))
+    .limit(pageSize)
+  }
+  postQuery.find().then(documents => {
+    fetchedPosts =documents
+    return Post.count();
+
+  }).then(count=>{
     res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-    });
+      message:"Post fecthed Successfully",
+      posts:fetchedPosts,
+      maxPosts: count
+    })
   });
 });
 
